46763b6bc4515799708c5c7ceec0a832c0c87dcd
dmowcomber <darthmowzy@gmail.com>
2017-02-18 21:17:54 -0800

    fix import

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
-import "gol"
+import "github.com/janaz/game-of-life-golang/gol"
 import "time"
 import "math/rand"
 
-
 func main() {
-  board := gol.NewBoard()
-  rand.Seed( time.Now().UTC().UnixNano())
-  for i := 0; i < 2410; i++ {
-    x := rand.Intn(130)
-    y := rand.Intn(38)
-    board.SetAlive(gol.NewPoint(x,y))
-  }
+	board := gol.NewBoard()
+	rand.Seed(time.Now().UTC().UnixNano())
+	for i := 0; i < 2410; i++ {
+		x := rand.Intn(130)
+		y := rand.Intn(38)
+		board.SetAlive(gol.NewPoint(x, y))
+	}
 
-  for {
-    board.Print(130,38)
-    time.Sleep(10 * time.Millisecond)
-    board = board.Next()
-  }
+	for {
+		board.Print(130, 38)
+		time.Sleep(10 * time.Millisecond)
+		board = board.Next()
+	}
 }
